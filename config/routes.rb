cc5d029fa191e80fbf64cafb8fd2288e7ea63082
Damien Nelson <dhnisaman@gmail.com>
2023-06-12 17:01:24 -0400

    add routes for session and user controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  get '/hello', to: 'application#hello_world'
+  
+  post '/signup', to: 'user#create'
+
+  post '/login', to: 'session#create'
+
+  get '/me', to: 'user#show'
+
+  delete '/logout', to: 'session#destroy'
 end
