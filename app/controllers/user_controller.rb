bcbbe23515a86274ed5b8ab7a67c53f0d8b9d803
Damien Nelson <dhnisaman@gmail.com>
2023-06-12 16:59:11 -0400

    create userController, add create, show methods

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,19 @@
+class UserController < ApplicationController
+    before_action :authorize, only: :show
+    def create 
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user, status: :created
+    end
+
+    def show
+        render json: @current_user
+    end
+
+
+    private
+
+    def user_params
+        params.permit(:username, :password, :password_comformation)
+    end
+end
