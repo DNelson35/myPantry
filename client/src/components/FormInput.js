92415633ff96dc8c54025bd6ebaaddc6a4d0f8b6
Damien Nelson <dhnisaman@gmail.com>
2023-06-12 17:08:23 -0400

    create formInput to create labels and input fields

---
+++ client/src/components/FormInput.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function FormInput({ title, input , onChange }) {
+    const capTitle = title.charAt(0).toUpperCase() + title.slice(1)
+  return (
+    <div className="mb-4">
+        <label className="block text-gray-700 text-sm font-bold mb-2">
+            {capTitle}
+        </label>
+        {title.includes('password')? 
+          <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" type="password" placeholder="******************" name={title} value={input} onChange={onChange}/>
+         :
+          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"  type="text" name={title} placeholder={capTitle} value={input} onChange={onChange} />
+        }
+    </div>
+  )
+}
+
+
+export default FormInput
