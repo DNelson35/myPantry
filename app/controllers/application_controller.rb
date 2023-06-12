974bc1807774752845c06fe3493185398239bb35
Damien Nelson <dhnisaman@gmail.com>
2023-06-12 16:57:11 -0400

    add in method authorize to check for active user.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,17 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
+    rescue_from ActiveRecord::RecordInvalid, with: :render_invalid
 
-    def hello_world
-        session[:count] = (session[:count] || 0) + 1
-        render json: { count: session[:count]}
+
+    private
+
+    def authorize 
+        @current_user = User.find_by(id: session[:user_id])
+
+        render json: {errors: ["Not Authorized"]}, status: :unauthorized unless session.include?(:user_id)
+    end
+
+    def render_invalid(e)
+        render json: {errors: e.record.errors.full_messages }, status: :unprocessable_entity
     end
 end
