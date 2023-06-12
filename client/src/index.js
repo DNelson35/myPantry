4e1f6849c415ee009a21a17838624d96e6be90e4
Damien Nelson <dhnisaman@gmail.com>
2023-06-12 17:04:12 -0400

    wrap application in context Provider

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Provider from './context/userContext';
 import { BrowserRouter as Router } from 'react-router-dom'
 import reportWebVitals from './reportWebVitals';
 
@@ -10,9 +11,11 @@ const el = document.getElementById('root')
 const root = ReactDOM.createRoot(el);
 
 root.render(
-  <Router>
-    <App/>
-  </Router>
+  <Provider>
+    <Router>
+      <App/>
+    </Router>
+  </Provider>
 )
 
 // If you want to start measuring performance in your app, pass a function
