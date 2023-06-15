722ed8e13edfaf50964c1501bf67d3c5b476c9e4
Damien Nelson <dhnisaman@gmail.com>
2023-06-15 13:03:35 -0400

    add error handling for RecordNotFound

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
+    
     rescue_from ActiveRecord::RecordInvalid, with: :render_invalid
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+
+    before_action :authorize
 
 
     private
@@ -13,5 +17,9 @@ class ApplicationController < ActionController::API
 
     def render_invalid(e)
         render json: {errors: e.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
+    def render_not_found
+        render json: {errors: 'Not Found'}, status: :not_found
     end
 end
