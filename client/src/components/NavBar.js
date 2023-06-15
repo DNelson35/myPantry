4e98d86980c0d6ba14fb45e60a731ed49873da06
Damien Nelson <dhnisaman@gmail.com>
2023-06-15 13:12:05 -0400

    update navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import { useState } from 'react'
+import { NavLink } from 'react-router-dom'
 import useUserContext from '../hooks/useUserContext'
 
 function NavBar() {
     const {user, setUser} = useUserContext()
+    const [search, setSearch] = useState('')
+
+    const onSearch = (e) => {
+        setSearch(e.target.value)
+    }
 
     const onLogOut = () => {
         fetch('/logout', {
@@ -16,11 +22,32 @@ function NavBar() {
     }
 
   return (
-    <nav className='flex relative w-screen h-20 bg-slate-500 align-middle'>
-        <h1 className='text-white text-2xl pl-3'>myPantry</h1>
-        <div className='inline-flex absolute right-3'>
-            <h3 className='capitalize text-white text-lg'>{user.username}</h3>
-            <button className='w-20 h-10 text-white bg-blue-800 rounded-full' onClick={onLogOut}>Log out</button>
+    <nav className="flex items-center justify-between flex-wrap bg-slate-500 py-4 lg:px-12 shadow border-solid border-t-2 border-slate-500">
+        <div className="flex justify-between lg:w-auto w-full lg:border-b-0 pl-6 pr-2 border-solid border-b-2 border-gray-300 pb-5 lg:pb-0">
+            <div className="flex items-center flex-shrink-0 text-white mr-16">
+                <span className="font-semibold text-2xl tracking-tight">myPantry</span>
+            </div>
+        </div>
+        <div className=" w-full flex-grow lg:flex lg:items-center lg:w-auto lg:px-3 px-8">
+            <div className="text-md font-bold text-white lg:flex-grow">
+                <NavLink
+                className="block mt-4 lg:inline-block lg:mt-0 hover:text-black px-4 py-2 mr-2">
+                    Home
+                </NavLink>
+            </div>
+            <div className="relative mx-auto text-gray-600 lg:block hidden">
+                <input className="border-2 border-gray-300 bg-white h-10 pl-2 pr-8 rounded-lg text-sm focus:outline-blue-500" placeholder="Search" value={search} onChange={onSearch}/>
+                <button type="submit" className="absolute right-0 top-0 mt-1 mr-2 text-xl">
+                🔍
+                </button>
+            </div>
+            <div className="flex ">
+                <h1
+                className="block text-md px-4 py-2 text-white ml-2 font-bold mt-4 lg:mt-0 capitalize">{user.name}</h1>
+    
+                <button
+                className=" block text-md px-4  ml-2 py-2 text-white font-bold hover:text-black mt-4 lg:mt-0" onClick={onLogOut}>Log Out</button>
+            </div>
         </div>
     </nav>
   )
