f3648cdf4a9425c31d2812c2424d75d4f0faf145
Damien Nelson <dhnisaman@gmail.com>
2023-06-20 13:46:21 -0400

    use new item serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :total_items
 
-  has_many :items
+  has_many :items, serializer: ItemWithExtraAttributesSerializer
   
   def total_items
     object.user_items.sum(:quantity)
