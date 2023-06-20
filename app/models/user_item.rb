287284696c9f511f4bae2c3200b3995c66251484
Damien Nelson <dhnisaman@gmail.com>
2023-06-20 13:46:41 -0400

    add validation

--- app/models/user_item.rb
+++ app/models/user_item.rb
@@ -1,4 +1,11 @@
 class UserItem < ApplicationRecord
     belongs_to :user
     belongs_to :item
+
+    validates :quantity, numericality: { greater_than: 0, allow_nil: true }, unless: :initial_quantity?
+
+    def initial_quantity?
+      quantity.nil? || quantity.zero?
+    end
+    
 end
